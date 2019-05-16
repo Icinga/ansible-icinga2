c8d012db2837bed498286824530cb55bb2249432
David Raison <david@tentwentyfour.lu>
2019-05-16 17:24:15 +0200

    Api users should not be distributed throughout the infrastructure. More complicated object attributes and templates.

--- filter_plugins/icinga2filters.py
+++ filter_plugins/icinga2filters.py
@@ -1,6 +1,6 @@
 import re
 
-icinga2_constants = [
+icinga2_keywords = [
   'NodeName',
   'ZoneName',
   'ConfigDir',
@@ -16,18 +16,31 @@ icinga2_constants = [
   'Critical',
   'Up',
   'Down',
+  'Problem',
+  'Acknowledgement',
+  'Recovery',
+  'Custom',
+  'FlappingStart',
+  'FlappingEnd',
+  'DowntimeStart',
+  'DowntimeEnd',
+  'DowntimeRemoved',
 ]
 
-def check_constants(value):
-  '''
-  Quotes values that do not represent icinga2 constants or match
+def check_keywords(value, iterator=None):
+  """
+  Quotes values that do not represent icinga2 keywords or match
   specific types or patterns, such as those representing host variables.
-  '''
+  Also leaves values unquoted that have appeared as keys or values in loops.
+  """
   if type(value) is int:
     return value
 
+  if type(iterator) is dict and (value == iterator['key'] or value == iterator['value']):
+    return value
+
   pattern = re.compile(r'^(?:\d+(ms|s|m|h|d)?|host\.vars.*)$')
-  if value in icinga2_constants or pattern.match(value):
+  if value in icinga2_keywords or pattern.match(value):
     return value
 
   return '"{}"'.format(value)
@@ -49,12 +62,12 @@ def check_key_format(key, prefix):
 
 
 class FilterModule(object):
-  '''
+  """
   custom jinja2 filters for working with icinga2
-  '''
+  """
 
   def filters(self):
     return {
-      'icinga_vars': check_constants,
+      'icinga_vars': check_keywords,
       'icingakey': check_key_format,
     }
